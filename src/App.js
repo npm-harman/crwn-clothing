9a8b346ee2ad8f8dc70d935d20dc3d8178f8e8a2
Harman Singh <harman.singh@nwea.org>
2021-06-11 13:01:56 +0530

    updated menu-item to navigate with withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsComponent = () => (
+  <div>
+    <h1>HATS COMPONENT</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop/hats' component={HatsComponent} />
+      </Switch>
     </div>
   );
 }
